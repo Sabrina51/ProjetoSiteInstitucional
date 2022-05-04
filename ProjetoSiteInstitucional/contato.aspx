ba4bb555ab0212160a7f6408478845d0dbe2c075
Sabrina Silva <sabrina.silva51@fatec.sp.gov.br>
2022-05-03 22:05:11 -0300

    add new pages

---
+++ ProjetoSiteInstitucional/contato.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/other.Master" AutoEventWireup="true" CodeBehind="contato.aspx.cs" Inherits="ProjetoSiteInstitucional.WebForm3" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <section class="container-fluid contato">
+        <div class="contato-content">
+            <div class="contato-title">
+                <h2>Fale Conosco</h2>
+            </div>
+            <div class="contato-fields">
+                <asp:Label ID="Msg" runat="server" ForeColor="red"></asp:Label>
+                <label>Mensagem</label>
+                <asp:TextBox ID="Mensagem" TextMode="MultiLine" MaxLength="256" Rows="6" runat="server"></asp:TextBox>
+
+                <label>Nome: </label>
+                <asp:TextBox ID="Nome" MaxLength="100" runat="server"></asp:TextBox>
+
+                <label>E-mail: </label>
+                <asp:TextBox ID="Email" MaxLength="256" runat="server"></asp:TextBox>
+
+                <asp:Button ID="Enviar" OnClick="Enviar_Click" runat="server" Text="Enviar" />
+            </div>
+        </div>
+    </section>
+</asp:Content>
---
+++ packages/ASD2022T/TratamentEexcecao.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASD2022T
+{
+
+    /// <summary>
+    /// TRATAMENTO DE ERRO
+    /// </summary>
+    /// 
+    public class TratamentEexcecao
+    {
+
+        private string caminhoFisico = System.Web.HttpContext.Current.Server.MapPath("~/Excecoes.txt");
+
+        /// <summary>
+        /// SALVA A EXCECAO EM UM DOCUMENTO TXT
+        /// </summary>
+        /// <param name="ex"></param>
+        public void SaveException(Exception ex)
+        {
+            string conteudo = "Data: " + DateTime.Now.ToString() + "\n";
+
+            conteudo += "Mensagem: " + ex.Message + "\n";
+            conteudo += "--------------------------\n";
+            System.IO.File.AppendAllText(caminhoFisico, conteudo);
+        }
+
+        /// <summary>
+        /// LE O ARQUIVO CONTENDO AS EXCECOES SALVAS
+        /// </summary>
+        /// <returns></returns>
+        public string LoadException()
+        {
+
+            return System.IO.File.ReadAllText(caminhoFisico);
+        }
+
+        /// <summary>
+        /// DELETA AS MENSAGENS ARMAZENADAS DO ARQUIVO DE EXCECOES
+        /// </summary>
+        public void DeleteException()
+        {
+
+            System.IO.File.Delete(caminhoFisico);
+        }
+    }
+}
